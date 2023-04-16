4887d74eec1fff514017d65a8b0c81e419381294
Juan <camejuanm@gmail.com>
2023-04-16 13:01:14 +0700

    put and delete added. bug fixes

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -5,17 +5,17 @@ const routes = require("./routes");
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: "localhost",
-        routes:{
-            cors:{
-                origin:["*"],
-            }
-        }
+        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+        routes: {
+            cors: {
+                origin: ["*"],
+            },
+        },
     });
-    
-    server.route(routes);
 
- 
+    server.route(routes);
+   
+   
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
